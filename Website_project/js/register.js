668d2fe042f21041ec4bb9ae05c3148eb1ec4e36
NguyenThiHien6 <20130257@st.hcmuaf.edu.vn>
2022-10-29 15:45:09 +0700

    add NguyenThiHien commit

---
+++ Website_project/js/register.js
@@ -0,0 +1,78 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+function checkInputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+    if (usernameValue == '') {
+        //error
+        //add error
+        setErrorFor(username, 'Tên người dùng không được bỏ trống');
+
+    } else {
+        setSuccessFor(username);
+
+    }
+    if (emailValue == '') {
+        setErrorFor(email, 'Email không được bỏ trống');
+
+
+    } else if (isEmail(emailValue) != true) {
+        setErrorFor(email, 'Email không hợp lệ');
+    } else {
+        setSuccessFor(email);
+    }
+
+   
+
+
+    if (passwordValue == '') {
+        setErrorFor(password, 'Mật khẩu không được bỏ trống');
+    } else if (!isPassword(passwordValue)) {
+        setErrorFor(password, 'Mật Khẩu không hợp lệ')
+    } else {
+        setSuccessFor(password);
+    }
+    if (password2Value == '') {
+        setErrorFor(password2, 'Mật khẩu không được bỏ trống');
+    } else if (!isPassword2(password2Value)) {
+        setErrorFor(password2, 'Mật Khẩu không hợp lệ')
+    } else {
+        setSuccessFor(password2);
+    }
+
+
+
+    function setErrorFor(input, message) {
+        const formControl = input.parentElement;
+        const small = formControl.querySelector('small');
+
+        small.innerText = message;
+
+        formControl.className = 'form-control error';
+    }
+    function setSuccessFor(input) {
+        const formControl = input.parentElement;
+        formControl.className = 'form-control success';
+
+    }
+    function isEmail(email) {
+        return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email);
+    }
+    function isPassword(password) {
+        return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password);
+    }
+    function isPassword2(password2) {
+        return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password2);
+    }
+}
