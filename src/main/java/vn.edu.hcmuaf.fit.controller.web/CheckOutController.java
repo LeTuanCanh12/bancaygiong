fe96f9f83492d2cabfc2dd12b2e243633f9a5fb0
DoanVietThong <115922012+DoanVietThong@users.noreply.github.com>
2023-01-04 00:46:31 +0700

    Update CheckOutController.java

--- src/main/java/vn.edu.hcmuaf.fit.controller.web/CheckOutController.java
+++ src/main/java/vn.edu.hcmuaf.fit.controller.web/CheckOutController.java
@@ -1,19 +1,16 @@
 package vn.edu.hcmuaf.fit.controller.web;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import vn.edu.hcmuaf.fit.dao.CategoryDAO;
 import vn.edu.hcmuaf.fit.dao.TransactionDao;
-import vn.edu.hcmuaf.fit.model.Product;
-import vn.edu.hcmuaf.fit.model.Transaction;
+import vn.edu.hcmuaf.fit.model.Cart;
 
 @WebServlet(urlPatterns = { "/thanh-toan" })
 public class CheckOutController extends HttpServlet {
@@ -25,19 +22,29 @@ public class CheckOutController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		TransactionDao dao = new TransactionDao();
-		resp.getWriter().write("end");
-
-		req.getRequestDispatcher("/views/web/checkOut.jsp").forward(req, resp);
-		
-
+		doPost(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
-	}
+//		req.getRequestDispatcher("/index.jsp").forward(req, resp);
+		TransactionDao dao = new TransactionDao();
+		HttpSession session = req.getSession();
 
+		Cart cart = (Cart) session.getAttribute("cart");
+		if (cart == null)
+			req.getRequestDispatcher("/views/web/checkOut.jsp").forward(req, resp);
+		else {
+			String name = req.getParameter("ho") + req.getParameter("ten");
+			String address = req.getParameter("diachi") + ", " + req.getParameter("thanhpho") + ", "
+					+ req.getParameter("quocgia");
+			String phone = req.getParameter("phone");
+			String email = req.getParameter("email");
+			String note = req.getParameter("note");
+
+			dao.createNewTransaction(name, address, phone, email, note, cart.tong_tien_gio());
+
+			req.getRequestDispatcher("/views/web/checkOut.jsp").forward(req, resp);
+		}
+	}
 }
